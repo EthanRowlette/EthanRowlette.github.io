e670eaaeb64e4f90423c8911a03aefd0248faffb
EthanRowlette <56183552+EthanRowlette@users.noreply.github.com>
2019-11-04 01:22:08 -0500

    Update contactForm.php

--- contactForm.php
+++ contactForm.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Connect Now</title>
-    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="/css/master.css">
     <?php include('php/master.php') ?>
   </head>
   <body class="darkMode">
