76a6caf4606716ca4fd7e271c3f2a1f3c9362e34
EthanRowlette <56183552+EthanRowlette@users.noreply.github.com>
2019-11-04 01:11:11 -0500

    Update master.php

--- PHP/master.php
+++ PHP/master.php
@@ -1,1 +1,18 @@
+<?php
+ if (ifset()$_POST['submit']) {
+   $name = $_POST['name'];
+   $subject = $_POST['subject'];
+   $mailFrom = $_POST['mail'];
+   $message = $_POST['message'];
+
+   $mailTo = "https://formspree.io/xoqaovaa"
+   $headers = "From: ".$mailFrom;
+   $txt = "you have recieved an E-mail from ".$name.".\n\n".$message;
+
+
+   mail($mailTo, $subject, $txt, $headers);
+   header("Location: contactForm.php?mailsend");
+
+ }
+ ?>
 
